5e0b26d5a993811e76627c4a35085b99524cd910
ltllml42 <63941428@qq.com>
2019-05-11 18:31:35 +0800

    增加Mobile登录页面
    Signed-off-by: ltllml42 <63941428@qq.com>

--- cap-sys/src/main/java/com/capinfo/camera/com/eparking/callback/IOnConnectStatus.java
+++
@@ -1,8 +0,0 @@
-package com.capinfo.camera.com.eparking.callback;
-
-import com.sun.jna.Callback;
-
-public interface IOnConnectStatus extends Callback
-{
-	public void callback(String chWTYIP, int nStatus, long dwUser);
-}
